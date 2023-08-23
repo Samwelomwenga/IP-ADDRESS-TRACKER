81b75379414282fd38a36acf74e0fcf9eda58340
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-08-23 11:58:11 +0300

    feat: add the vertical line  before location, timezone, isp
    Add the vertical line  before location, timezone, isp in the IP address
    details component to much the design

--- src/components/IPAddressDetailes.jsx
+++ src/components/IPAddressDetailes.jsx
@@ -2,35 +2,55 @@ import PropsTypes from "prop-types";
 import {
   StyledDetaile,
   StyledContainerSection,
-  StyledSection,
+StyledSection,
   StyledH6,
   StyledSpan,
+  StyledHr,
 } from "./styled/IPAddressDetailes.Styled";
-const IPAddressDetailes = ({geoLocation,loading}) => {
-  const {as:{asn},ip,location:{country,city,timezone},isp}=geoLocation;
+const IPAddressDetailes = ({ geoLocation, loading }) => {
+  const {
+    as: { asn },
+    ip,
+    location: { country, city, timezone },
+    isp,
+  } = geoLocation;
   return (
     loading && <h1>Loading...</h1>,
-    <StyledContainerSection>
-      <StyledSection>
-        <StyledH6> IP ADDRESS</StyledH6>
-        <StyledDetaile>{ip}</StyledDetaile>
-      </StyledSection>
+    (
+      <StyledContainerSection>
+        <section>
+          <StyledH6> IP ADDRESS</StyledH6>
+          <StyledDetaile>{ip}</StyledDetaile>
+        </section>
 
-      <section>
-        <StyledH6>LOCATION</StyledH6>
-        <StyledDetaile>
-          {city?city:"_"}{", "}{country?country:"_"}{' '}<StyledSpan>{ asn? asn:"_"}</StyledSpan>
-        </StyledDetaile>
-      </section>
-      <section>
-        <StyledH6>TIMEZONE</StyledH6>
-        <StyledDetaile>UTC {timezone?timezone:"_"}</StyledDetaile>
-      </section>
-      <section>
+        <section style={{display:"flex"}}>
+        <StyledHr></StyledHr>
+      <div>
+      <StyledH6>LOCATION</StyledH6>
+          <StyledDetaile>
+            {city ? city : "_"}
+            {", "}
+            {country ? country : "_"} <StyledSpan>{asn ? asn : "_"}</StyledSpan>
+          </StyledDetaile>
+      </div>
+        </section>
+        <StyledSection>
+        <StyledHr></StyledHr>
+
+         <div>
+         <StyledH6>TIMEZONE</StyledH6>
+          <StyledDetaile>UTC {timezone ? timezone : "_"}</StyledDetaile>
+         </div>
+        </StyledSection>
+        <StyledSection>
+          <StyledHr></StyledHr>
+        <div>
         <StyledH6>ISP</StyledH6>
-        <StyledDetaile>{isp?isp:"_"}</StyledDetaile>
-      </section>
-    </StyledContainerSection>
+          <StyledDetaile>{isp ? isp : "_"}</StyledDetaile>
+        </div>
+        </StyledSection>
+      </StyledContainerSection>
+    )
   );
 };
 
@@ -38,8 +58,5 @@ IPAddressDetailes.propTypes = {
   geoLocation: PropsTypes.object.isRequired,
   loading: PropsTypes.bool.isRequired,
 };
-
-
-
 
 export default IPAddressDetailes;
--- src/components/styled/IPAddressDetailes.Styled.jsx
+++ src/components/styled/IPAddressDetailes.Styled.jsx
@@ -23,31 +23,35 @@ export const StyledContainerSection=styled.section`
     }
 `
 export const StyledSection=styled.section`
-    /* &::before{
-          content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1' height='100'%3E%3Cline x1='0' y1='0' x2='0' y2='100' stroke='black' /%3E%3C/svg%3E");
-        display: inline;
-        height: 100px;
-        border-left: 1px;
-    } */
+   display: flex;
+   
 `
 export const StyledH6=styled.h6`
-    color: hsl(0, 0%, 59%);
+    color: rgb(150, 150, 150);
     font-weight: 500;
 font-size: small;
+margin-bottom: .4rem;
 `
 export const StyledDetaile=styled.p`
     color: hsl(0, 0%, 17%);
     font-weight: 700;
-    font-size: larger;
+    
 `
 export const StyledSpan=styled.span`
     @media (min-width: 768px) {
         display: block;
+        margin-top: .3rem;
+       
+
     }
 `
-// export const StyledHr=styled.div`
-// visibility: hidden;
-//     @media (min-width: 768px) {
-//        visibility: visible;
-//     }
-// `
+export const StyledHr=styled.div`
+background-color: hsl(0, 0%, 58.82352941176471%);
+width: 1px;
+height: 4rem;
+margin-right: 1rem;
+visibility: hidden;
+     @media (min-width: 768px) {
+      visibility: visible;
+    }
+`
